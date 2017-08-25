f75eecd18f2e418e2ea86b5eeefd4c0f8161aa35
Thiago Melo <thiago.lc.melo@gmail.com>
2017-08-25 03:24:53 -0300

    prior to alpha

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ For uploading a new version it is necessary to update the versions inside `setup
 version = '0.1.5'
 ```
 
-In case of a change in the [oglhslack](https://github.com/opengeardev/oglhslack), it is also necessary to update the `download_url` variable in `setup.py` for matching the proper reliese.
+In case of a change in the [oglhslack](https://github.com/opengeardev/oglhslack), it is also necessary to update the `download_url` variable in `setup.py` for matching the proper release.
 
 ## Uploading
 
@@ -16,5 +16,6 @@ For uploading a new version:
 
 ```bash
 $ git submodule update --remote
+$ pandoc --from=markdown --to=rst --output=README.rst oglhslack/README.md
 $ python setup.py sdist upload
 ```
--- setup.py
+++ setup.py
@@ -6,21 +6,21 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Translate oglhslack README.md to .rst
-long_description = pypandoc.convert_file(path.join(here, 'oglhslack/README.md'), 'rst')
+#long_description = pypandoc.convert_file(path.join(here, 'oglhslack/README.md'), 'rst')
 
 # Get the long description from the relevant file
-#with io.open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-#    long_description = f.read()
+with io.open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
   name = 'oglhslack',
-  version = '0.1.5.3',
+  version = '0.9',
   description = 'A Slack bot client for Lighthouse API',
   long_description=long_description,
   author = 'Lighthouse Team',
   author_email = 'engineering@opengear.com',
   url = 'https://github.com/opengeardev/oglhslack',
-  download_url = 'https://github.com/opengeardev/oglhslack/archive/0.1.5.2.tar.gz',
+  download_url = 'https://github.com/opengeardev/oglhslack/archive/0.9.tar.gz',
   keywords = ['api', 'opengear', 'lighthouse', 'slackbot'],
   classifiers = [
     'Development Status :: 3 - Alpha',
